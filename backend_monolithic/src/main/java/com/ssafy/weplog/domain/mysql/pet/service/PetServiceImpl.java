e074a23612beeb65cb4fe5b359e39ed1091c3df0
2sy0127 <2sy0127@naver.com>
2022-10-25 13:47:20 +0900

    feat: MemberPet Controller, Service, Repository

--- backend_monolithic/src/main/java/com/ssafy/weplog/domain/mysql/pet/service/PetServiceImpl.java
+++ backend_monolithic/src/main/java/com/ssafy/weplog/domain/mysql/pet/service/PetServiceImpl.java
@@ -2,6 +2,13 @@ package com.ssafy.weplog.domain.mysql.pet.service;
 
 import java.util.List;
 
+import com.ssafy.weplog.domain.mysql.member.dao.MemberRepository;
+import com.ssafy.weplog.domain.mysql.member.domain.Member;
+import com.ssafy.weplog.domain.mysql.memberpet.dao.MemberPetRepository;
+import com.ssafy.weplog.domain.mysql.memberpet.domain.MemberPet;
+import com.ssafy.weplog.domain.mysql.pet.dao.PetRepository;
+import com.ssafy.weplog.domain.mysql.pet.dto.PetReq;
+import com.ssafy.weplog.domain.mysql.pet.dto.PetRes;
 import org.springframework.stereotype.Service;
 
 import com.ssafy.weplog.domain.mysql.pet.domain.Pet;
@@ -13,13 +20,16 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @RequiredArgsConstructor
 public class PetServiceImpl implements PetService {
+
+	private static PetRepository petRepository;
+	private static MemberRepository memberRepository;
+	private static MemberPetRepository memberPetRepository;
+
 	@Override
-	public List<Pet> getMyPets(String userId) {
+	public List<PetRes> getPetsByLevel(int level) {
+
 		return null;
 	}
 
-	@Override
-	public List<Pet> getPetsByLevel(int level) {
-		return null;
-	}
+
 }
