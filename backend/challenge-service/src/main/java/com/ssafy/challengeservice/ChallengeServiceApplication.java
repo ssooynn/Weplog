78fdd141d13ab2387d852729833a36306c56e4c4
rjeowornjs2 <rjeowornjs@naver.com>
2022-10-31 09:46:42 +0900

    feat : MSA 기본 구조 세팅

---
+++ backend/challenge-service/src/main/java/com/ssafy/challengeservice/ChallengeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.challengeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeServiceApplication.class, args);
+    }
+
+}
