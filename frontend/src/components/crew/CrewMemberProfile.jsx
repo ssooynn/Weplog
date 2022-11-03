bfb3657a8a5e18cac006269c8fc4c201fd40f254
ahyun98 <dldkgus98@naver.com>
2022-11-03 16:46:18 +0900

    feat: 크루 페이지 - Talk, member 디자인 구현

---
+++ frontend/src/components/crew/CrewMemberProfile.jsx
@@ -0,0 +1,51 @@
+import { Box, Text } from 'grommet'
+import React from 'react'
+import styled from 'styled-components'
+import { StyledProfile } from '../common/ProfileImg'
+
+const StyledHorizonTable = styled.div`
+    width: 100%;
+    overflow-x:scroll ;
+    overflow-y: hidden;
+    white-space: nowrap;
+    &::-webkit-scrollbar {
+    display: none;
+    }
+    .card {
+        display: inline-block;
+    }
+`
+
+export const CrewMemberProfile = () => {
+    return (
+        <StyledHorizonTable>
+            <div className="card">
+                <Box direction='column' justify='between' align='center' margin='5px 7px 5px 0px'>
+                    <StyledProfile height='60px' width='60px' style={{ objectFit: 'cover' }} src='https://picsum.photos/200/300?random=43' />
+                    <Text size='14px' weight='500'>이아현</Text>
+                </Box>
+            </div>
+            <div className="card">
+                <Box direction='column' justify='between' align='center' margin='5px 10px 5px 0px'>
+                    <StyledProfile height='60px' width='60px' style={{ objectFit: 'cover' }} src='https://picsum.photos/200/300?random=43' />
+                    <Text size='14px' weight='500'>이아현</Text>
+                </Box>
+            </div><div className="card">
+                <Box direction='column' justify='between' align='center' margin='5px 10px 5px 0px'>
+                    <StyledProfile height='60px' width='60px' style={{ objectFit: 'cover' }} src='https://picsum.photos/200/300?random=43' />
+                    <Text size='14px' weight='500'>이아현</Text>
+                </Box>
+            </div><div className="card">
+                <Box direction='column' justify='between' align='center' margin='5px 10px 5px 0px'>
+                    <StyledProfile height='60px' width='60px' style={{ objectFit: 'cover' }} src='https://picsum.photos/200/300?random=43' />
+                    <Text size='14px' weight='500'>이아현</Text>
+                </Box>
+            </div><div className="card">
+                <Box direction='column' justify='between' align='center' margin='5px 10px 5px 0px'>
+                    <StyledProfile height='60px' width='60px' style={{ objectFit: 'cover' }} src='https://picsum.photos/200/300?random=43' />
+                    <Text size='14px' weight='500'>이아현</Text>
+                </Box>
+            </div>
+        </StyledHorizonTable>
+    )
+}
