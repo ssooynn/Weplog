a64036c154a56eff93e34ee5c48073126f42a643
2sy0127 <2sy0127@naver.com>
2022-11-03 14:32:22 +0900

    feat: MemberService, Repository

--- backend/member-service/src/main/java/com/ssafy/memberservice/domain/member/service/MemberServiceImpl.java
+++ backend/member-service/src/main/java/com/ssafy/memberservice/domain/member/service/MemberServiceImpl.java
@@ -1,12 +1,41 @@
 package com.ssafy.memberservice.domain.member.service;
 
+import com.ssafy.memberservice.domain.member.dao.MemberRepository;
+import com.ssafy.memberservice.domain.member.domain.Member;
+import com.ssafy.memberservice.domain.member.dto.MemberReq;
+import com.ssafy.memberservice.global.common.error.exception.NotExistException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class MemberServiceImpl {
+public class MemberServiceImpl implements MemberService{
 
+    private static MemberRepository memberRepository;
+
+    @Override
+    public void updateMemberInfo(UUID id, MemberReq memberReq) {
+        Member member = memberRepository.findById(id).orElseThrow(() -> new NotExistException("해당 아이디가 존재하지 않습니다."));
+        member.updateMember(memberReq);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public boolean checkDoubleNickname(String nickname) {
+        Optional<Member> member = memberRepository.findByNickname(nickname);
+        return member.isPresent();
+    }
+
+    @Override
+    public void postMyPet(UUID memberId, Long petId) {
+
+    }
 }
