0a253fbb33e5db5505fcf098d99b16b34cab2f73
2sy0127 <2sy0127@naver.com>
2022-11-03 11:04:51 +0900

    feat: MyPage

--- backend/member-service/src/main/java/com/ssafy/memberservice/domain/memberdetail/dao/MemberDetailRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.memberservice.domain.memberdetail.dao;
-
-import com.ssafy.memberservice.domain.memberdetail.domain.MemberDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberDetailRepository extends JpaRepository<MemberDetail, Long> {
-}
