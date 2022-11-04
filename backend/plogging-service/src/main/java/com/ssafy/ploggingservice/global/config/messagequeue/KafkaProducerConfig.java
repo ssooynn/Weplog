aae912a0395c650f9b4061a1d80517ba81c22e54
rjeowornjs2 <rjeowornjs@naver.com>
2022-11-04 10:50:23 +0900

    fix : 배포 확인 완료

--- backend/plogging-service/src/main/java/com/ssafy/ploggingservice/global/config/messagequeue/KafkaProducerConfig.java
+++ backend/plogging-service/src/main/java/com/ssafy/ploggingservice/global/config/messagequeue/KafkaProducerConfig.java
@@ -2,6 +2,7 @@ package com.ssafy.ploggingservice.global.config.messagequeue;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -16,10 +17,13 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfig {
 
+    @Value("${kafka-ip}")
+    private String kafkaIp;
+
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> properties = new HashMap<>();
-        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaIp);
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
