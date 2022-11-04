5a0fd8c45c79e69485032591354d75495b60f8c4
2sy0127 <2sy0127@naver.com>
2022-11-04 17:52:56 +0900

    feat: Calendar

---
+++ backend/member-service/src/main/java/com/ssafy/memberservice/domain/calendar/repository/CalendarRepository.java
@@ -0,0 +1,14 @@
+package com.ssafy.memberservice.domain.calendar.repository;
+
+import com.ssafy.memberservice.domain.calendar.domain.Calendar;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface CalendarRepository extends JpaRepository<Calendar, Long> {
+
+    @Query("select c from Calendar c where c.crew.id=:crewId and c.scheduleDate.getMonthValue=:time.getMothValue")
+    List<Calendar> findAllByDate(LocalDateTime time, Long crewId);
+}
