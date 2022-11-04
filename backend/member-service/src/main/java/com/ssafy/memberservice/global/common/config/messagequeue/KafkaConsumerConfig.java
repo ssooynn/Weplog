dd02750a945a5ce8ff143aeaa2d01b50ca85b00a
rjeowornjs2 <rjeowornjs@naver.com>
2022-11-04 10:50:23 +0900

    fix : 배포 확인 완료

--- backend/member-service/src/main/java/com/ssafy/memberservice/global/common/config/messagequeue/KafkaConsumerConfig.java
+++ backend/member-service/src/main/java/com/ssafy/memberservice/global/common/config/messagequeue/KafkaConsumerConfig.java
@@ -2,6 +2,7 @@ package com.ssafy.memberservice.global.common.config.messagequeue;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -16,10 +17,12 @@ import java.util.Map;
 @Configuration
 public class KafkaConsumerConfig {
 
+    @Value("${kafka-ip}")
+    private String kafkaIp;
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> properties = new HashMap<>();
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaIp);
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, "consumerGroupId");
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
