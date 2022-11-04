4921d575f5d11a73b8397eef2b3dc979691b0b30
rjeowornjs2 <rjeowornjs@naver.com>
2022-11-04 20:55:10 +0900

    feat : member-service 크루기능 개발 및 CORS 설정

--- backend/member-service/src/main/java/com/ssafy/memberservice/domain/crew/dao/CrewRepository.java
+++ backend/member-service/src/main/java/com/ssafy/memberservice/domain/crew/dao/CrewRepository.java
@@ -2,6 +2,17 @@ package com.ssafy.memberservice.domain.crew.dao;
 
 import com.ssafy.memberservice.domain.crew.domain.Crew;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface CrewRepository extends JpaRepository<Crew, Long> {
+
+    @Query("select distinct c from Crew c join fetch c.crewMaster join c.memberCrewList where c.id = :crewId")
+    Optional<Crew> findByIdWithCrewMasterAndParticipants(Long crewId);
+
+    @Query("select distinct c from Crew c join fetch c.memberCrewList cl join fetch cl.member")
+    List<Crew> findAllWithMemberCrewList();
 }
--- backend/member-service/src/main/java/com/ssafy/memberservice/domain/crew/dto/CreateCrewRequest.java
+++ backend/member-service/src/main/java/com/ssafy/memberservice/domain/crew/dto/CreateCrewRequest.java
@@ -5,10 +5,15 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateCrewRequest {
 
     private String name;
     private String description;
     private String activityArea;
+    private Integer maxParticipantCnt;
+    private Double lat;
+    private Double lon;
 
 }
