75f2bf877ba3ae339237e367ffb194e2f0611276
wornjs135 <rjeowornjs@naver.com>
2022-11-05 12:36:14 +0900

    feat : 내 근처 크루 목록, 내 크루 목록 조회 추가

--- backend/member-service/src/main/java/com/ssafy/memberservice/domain/crew/service/CrewService.java
+++ backend/member-service/src/main/java/com/ssafy/memberservice/domain/crew/service/CrewService.java
@@ -2,6 +2,7 @@ package com.ssafy.memberservice.domain.crew.service;
 
 import com.ssafy.memberservice.domain.crew.dto.*;
 import com.ssafy.memberservice.global.security.auth.CustomUserDetails;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -21,4 +22,8 @@ public interface CrewService {
     List<CrewSimpleResponse> getCrewList();
 
     Top3CrewResponse getTop3CrewList();
+
+    List<CrewSimpleResponse> getCrewListNear(Double lat, Double lon);
+
+    List<CrewSimpleResponse> getMyCrewList(UUID memberId);
 }
