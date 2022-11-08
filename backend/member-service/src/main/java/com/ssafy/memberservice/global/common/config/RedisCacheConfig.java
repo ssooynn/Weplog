67c571bd9e29f330b829670741f8393b50feb80b
qospwmf <qospwmf@gmail.com>
2022-11-08 13:43:38 +0900

    feat: 채팅 1차 구현

--- backend/member-service/src/main/java/com/ssafy/memberservice/global/common/config/RedisCacheConfig.java
+++ backend/member-service/src/main/java/com/ssafy/memberservice/global/common/config/RedisCacheConfig.java
@@ -5,6 +5,7 @@ import com.ssafy.memberservice.domain.chatting.pubsub.PloggingChatSubscriber;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.redis.cache.CacheKeyPrefix;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
@@ -22,6 +23,7 @@ import java.util.Map;
 
 @Configuration
 @EnableCaching
+@EnableJpaRepositories
 public class RedisCacheConfig {
 
     @Bean
