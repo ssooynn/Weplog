d545565e138ead34ded7fc9cc25f7ad630fe38f5
2sy0127 <2sy0127@naver.com>
2022-11-08 14:11:59 +0900

    fix: get plogging POINT

--- backend/plogging-service/src/main/java/com/ssafy/ploggingservice/service/PloggingService.java
+++ backend/plogging-service/src/main/java/com/ssafy/ploggingservice/service/PloggingService.java
@@ -5,7 +5,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -14,7 +13,7 @@ public interface PloggingService {
     CreatePloggingRes postPloggingPicture(UUID memberId, Long ploggingId, MultipartFile image);
     Slice<PloggingRes> getPloggingList(UUID memberId, Pageable pageable);
     List<GarbageDto> getTrashCansLoc(double lat, double lng);
-    ArrayList<ArrayList<CoordinateDto>> getPloggingLoc(double lat, double lng);
+    List<List<CoordinateDto>> getPloggingLoc(double lat, double lng);
     PloggingDetailRes getPloggingInfo(Long id);
 
 
