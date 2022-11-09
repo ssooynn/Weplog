a7eb8285b8f96390a56855c9940daf5ab56de8eb
2sy0127 <2sy0127@naver.com>
2022-11-09 16:40:52 +0900

    fix: Calendar LocalDateTime to LocalDate

--- backend/member-service/src/main/java/com/ssafy/memberservice/domain/calendar/service/CalendarService.java
+++ backend/member-service/src/main/java/com/ssafy/memberservice/domain/calendar/service/CalendarService.java
@@ -3,10 +3,12 @@ package com.ssafy.memberservice.domain.calendar.service;
 import com.ssafy.memberservice.domain.calendar.dto.CalendarReq;
 import com.ssafy.memberservice.domain.calendar.dto.CalendarRes;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public interface CalendarService {
-    CalendarRes getCalendarInfo(LocalDateTime time, Long crewId);
+    List<CalendarRes> getCalendarInfo(LocalDate time, Long crewId);
     void postCalendarInfo(UUID memberId, CalendarReq calendarReq);
 }
