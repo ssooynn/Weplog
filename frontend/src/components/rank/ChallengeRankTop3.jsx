7434e5c018d8a213c394d0e642b26aa03cf7483d
문석희 <zazachucky@naver.com>
2022-11-09 16:29:09 +0900

    feat : crew API 연결, 플로깅 채팅 완료

--- frontend/src/components/rank/ChallengeRankTop3.jsx
+++ frontend/src/components/rank/ChallengeRankTop3.jsx
@@ -27,59 +27,61 @@ const Profile3rd = styled.img`
   border: 2px solid #ff9900;
 `;
 
-export const ChallengeRankTop3 = () => {
-  return (
-    <Box
-      direction="row"
-      justify="evenly"
-      width="100%"
-      align="end"
-      margin="20px 0px"
-    >
-      <Box direction="column" height="85px" justify="between" align="center">
-        <Profile2nd src="https://picsum.photos/400/300" alt="프로필 사진" />
-        <Box direction="column">
-          <Text size="10px" weight={400}>
-            문석희
-          </Text>
-          <Text size="10px" weight={400}>
-            24.5%
-          </Text>
-        </Box>
-      </Box>
+export const ChallengeRankTop3 = ({ top3Crews }) => {
+  if (top3Crews && top3Crews.length > 0)
+    return (
       <Box
-        direction="column"
-        height="120px"
-        justify="between"
-        align="center"
-        style={{ position: "relative" }}
+        direction="row"
+        justify="evenly"
+        width="100%"
+        align="end"
+        margin="20px 0px"
       >
-        <img
-          src={crown}
-          alt="왕관 사진"
-          style={{ position: "absolute", top: "-20px", left: "30px" }}
-        />
-        <Profile1st src="https://picsum.photos/400/300" alt="프로필 사진" />
-        <Box direction="column" align="center">
-          <Text size="12px" weight={500}>
-            권기정
-          </Text>
-          <Text size="10px" weight={400}>
-            24.5%
-          </Text>
+        <Box direction="column" height="85px" justify="between" align="center">
+          <Profile2nd src={top3Crews[1].imageUrl} alt="프로필 사진" />
+          <Box direction="column">
+            <Text size="10px" weight={400}>
+              {top3Crews[1].name}
+            </Text>
+            <Text size="10px" weight={400}>
+              {top3Crews[1].totalAmount}
+            </Text>
+          </Box>
+        </Box>
+        <Box
+          direction="column"
+          height="120px"
+          justify="between"
+          align="center"
+          style={{ position: "relative" }}
+        >
+          <img
+            src={crown}
+            alt="왕관 사진"
+            style={{ position: "absolute", top: "-20px", left: "30px" }}
+          />
+          <Profile1st src={top3Crews[0].imageUrl} alt="프로필 사진" />
+          <Box direction="column" align="center">
+            <Text size="12px" weight={500}>
+              {top3Crews[0].name}
+            </Text>
+            <Text size="10px" weight={400}>
+              {top3Crews[0].totalAmount}
+            </Text>
+          </Box>
+        </Box>
+        <Box direction="column" height="85px" justify="between" align="center">
+          <Profile3rd src={top3Crews[2].imageUrl} alt="프로필 사진" />
+          <Box direction="column">
+            <Text size="10px" weight={400}>
+              {top3Crews[2].name}
+            </Text>
+            <Text size="10px" weight={400}>
+              {top3Crews[2].totalAmount}
+            </Text>
+          </Box>
         </Box>
       </Box>
-      <Box direction="column" height="85px" justify="between" align="center">
-        <Profile3rd src="https://picsum.photos/400/300" alt="프로필 사진" />
-        <Box direction="column">
-          <Text size="10px" weight={400}>
-            문석희
-          </Text>
-          <Text size="10px" weight={400}>
-            24.5%
-          </Text>
-        </Box>
-      </Box>
-    </Box>
-  );
+    );
+  else return <Box>없어요</Box>;
 };
--- frontend/src/pages/crew/Crew.jsx
+++ frontend/src/pages/crew/Crew.jsx
@@ -2,86 +2,148 @@ import React, { useEffect, useState } from "react";
 import { CrewCard } from "../../components/crew/CrewCard";
 import crewBanner from "../../assets/images/crewBanner.jpg";
 import { motion } from "framer-motion";
-import { Box, Text } from "grommet";
+import { Box, Spinner, Text } from "grommet";
 import { CrewScollableCardList } from "../../components/crew/CrewScollableCardList";
 import { ChallengeRankTop3 } from "../../components/rank/ChallengeRankTop3";
+import {
+  getMyCrewList,
+  getMyNearCrewList,
+  getTop3CrewList,
+} from "../../apis/crewApi";
 
 export const Crew = () => {
-  const [top3, setTop3] = useState([]);
+  const [top3Distance, setTop3Distance] = useState([]);
+  const [top3Time, setTop3Time] = useState([]);
+  const [myCrews, setMyCrews] = useState([]);
+  const [near, setNear] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [type, setType] = useState("DIS");
 
-  useEffect(() => {}, []);
-  return (
-    <motion.div>
-      <div style={{ position: "relative", width: "100%", height: "260px" }}>
-        <img
-          src={crewBanner}
-          height="260px"
-          width="100%"
-          style={{ position: "absolute", objectFit: "cover" }}
-        ></img>
-        <Box
-          background={{ color: "rgba(0,0,0,0.2)" }}
-          style={{ position: "absolute" }}
-          width="100%"
-          height="100%"
-        ></Box>
-        <Text
-          size="26px"
-          weight={500}
-          color="white"
-          style={{ position: "absolute", top: "160px", left: "20px" }}
-        >
-          친구들과 함께하는
-        </Text>
-        <Box
-          direction="row"
-          style={{ position: "absolute", top: "200px", left: "20px" }}
-          align="center"
-        >
-          <Text size="26px" weight={500} color="white">
-            지구 지키기
-          </Text>
-          <Text size="32px" weight={600} color="white" margin="0px 10px">
-            Crew
-          </Text>
-        </Box>
-      </div>
-      {/* 내 크루 */}
-      <Box pad="medium" width="100%">
-        <Text size="18px" weight={500} margin="20px 10px 0px 10px">
-          내 크루
-        </Text>
-        <CrewScollableCardList></CrewScollableCardList>
-      </Box>
+  const handleType = (type) => {
+    setType(type);
+  };
 
-      {/* 지금 내 위치가 주 활동 지역인 근처 크루 */}
-      <Box pad="medium" width="100%">
-        <Text size="18px" weight={500} margin="20px 10px 0px 10px">
-          지금 내 근처 크루
-        </Text>
-        <CrewScollableCardList></CrewScollableCardList>
-      </Box>
-
-      {/* Top3 */}
-      <Box pad="medium" width="100%">
-        <Box direction="row" justify="between" align="center">
-          <Text size="18px" weight={500} margin="20px 10px 30px 10px">
-            크루 Top3
+  useEffect(() => {
+    if (loading) {
+      getTop3CrewList(
+        (response) => {
+          console.log(response);
+          setTop3Distance((prev) => (prev = response.data.top3Distance));
+          setTop3Time((prev) => (prev = response.data.top3Time));
+        },
+        (fail) => {
+          console.log(fail);
+        }
+      );
+      getMyCrewList((response) => {
+        console.log(response);
+        setMyCrews((prev) => (prev = response.data));
+        setLoading(false);
+      });
+      // navigator.geolocation.getCurrentPosition(
+      //   (position) => {
+      //     console.log(position);
+      //     getMyNearCrewList(
+      //       { lat: position.coords.latitude, lng: position.coords.longitude },
+      //       (response) => {
+      //         console.log(response);
+      //         setNear((prev) => (prev = response.data));
+      //       }
+      //     );
+      //   },
+      //   (err) => {}
+      // );
+    }
+    setLoading(false);
+    return () => {
+      setLoading(false);
+    };
+  }, []);
+  if (loading) return <Spinner />;
+  else
+    return (
+      <motion.div>
+        <div style={{ position: "relative", width: "100%", height: "260px" }}>
+          <img
+            src={crewBanner}
+            height="260px"
+            width="100%"
+            style={{ position: "absolute", objectFit: "cover" }}
+          ></img>
+          <Box
+            background={{ color: "rgba(0,0,0,0.2)" }}
+            style={{ position: "absolute" }}
+            width="100%"
+            height="100%"
+          ></Box>
+          <Text
+            size="26px"
+            weight={500}
+            color="white"
+            style={{ position: "absolute", top: "160px", left: "20px" }}
+          >
+            친구들과 함께하는
           </Text>
-          <Box direction="row" justify="between" width="90px">
-            <Text weight={400} size="12px" color="#57BA83">
-              총 횟수
+          <Box
+            direction="row"
+            style={{ position: "absolute", top: "200px", left: "20px" }}
+            align="center"
+          >
+            <Text size="26px" weight={500} color="white">
+              지구 지키기
             </Text>
-            <Text weight={400} size="12px" color="#AEAEAE">
-              |
-            </Text>
-            <Text weight={400} size="12px" color="#AEAEAE">
-              총 거리
+            <Text size="32px" weight={600} color="white" margin="0px 10px">
+              Crew
             </Text>
           </Box>
+        </div>
+        {/* 내 크루 */}
+        <Box pad="medium" width="100%">
+          <Text size="18px" weight={500} margin="20px 10px 0px 10px">
+            내 크루
+          </Text>
+          {myCrews && <CrewScollableCardList crews={myCrews} />}
         </Box>
-        <ChallengeRankTop3 />
-      </Box>
-    </motion.div>
-  );
+
+        {/* 지금 내 위치가 주 활동 지역인 근처 크루 */}
+        <Box pad="medium" width="100%">
+          <Text size="18px" weight={500} margin="20px 10px 0px 10px">
+            지금 내 근처 크루
+          </Text>
+          <CrewScollableCardList></CrewScollableCardList>
+        </Box>
+
+        {/* Top3 */}
+        <Box pad="medium" width="100%">
+          <Box direction="row" justify="between" align="center">
+            <Text size="18px" weight={500} margin="20px 10px 30px 10px">
+              크루 Top3
+            </Text>
+            <Box direction="row" justify="between" width="90px">
+              <Text
+                weight={400}
+                size="12px"
+                color={type === "TIME" ? "#57BA83" : "#AEAEAE"}
+                onClick={() => handleType("TIME")}
+              >
+                총 횟수
+              </Text>
+              <Text weight={400} size="12px" color="#AEAEAE">
+                |
+              </Text>
+              <Text
+                weight={400}
+                size="12px"
+                color={type === "DIS" ? "#57BA83" : "#AEAEAE"}
+                onClick={() => handleType("DIS")}
+              >
+                총 거리
+              </Text>
+            </Box>
+          </Box>
+          {type === "TIME" && <ChallengeRankTop3 top3Crews={top3Time} />}
+          {type === "DIS" && <ChallengeRankTop3 top3Crews={top3Distance} />}
+        </Box>
+      </motion.div>
+    );
 };
