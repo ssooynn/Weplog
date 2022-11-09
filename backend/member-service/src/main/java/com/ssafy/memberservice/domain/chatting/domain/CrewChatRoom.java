af1575c06f1eb58f21a3ce3eb48d7cb61a0972a7
qospwmf <qospwmf@gmail.com>
2022-11-09 13:46:55 +0900

    fix: 크루 채팅 방 구현, 현재 진행중인 크루 플로깅 방 api 추가

--- backend/member-service/src/main/java/com/ssafy/memberservice/domain/chatting/domain/CrewChatRoom.java
+++ backend/member-service/src/main/java/com/ssafy/memberservice/domain/chatting/domain/CrewChatRoom.java
@@ -1,5 +1,7 @@
 package com.ssafy.memberservice.domain.chatting.domain;
 
+import com.ssafy.memberservice.domain.member.domain.Member;
+import com.ssafy.memberservice.domain.membercrew.domain.MemberCrew;
 import com.ssafy.memberservice.global.common.base.BaseEntity;
 import lombok.Getter;
 import org.springframework.data.redis.core.RedisHash;
@@ -7,6 +9,7 @@ import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.annotation.Id;
 
 import java.io.Serializable;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @Getter
@@ -19,5 +22,12 @@ public class CrewChatRoom implements Serializable {
     private Map<String, Participant> playerMap;
 
     private String chatId;
+
+    public static CrewChatRoom create(Long crewId, MemberCrew memberCrew) {
+        CrewChatRoom crewChatRoom = new CrewChatRoom();
+        crewChatRoom.roomId = crewId;
+        crewChatRoom.playerMap = new LinkedHashMap<>();
+        return crewChatRoom;
+    }
 
 }
--- backend/member-service/src/main/java/com/ssafy/memberservice/domain/chatting/service/PloggingChatService.java
+++ backend/member-service/src/main/java/com/ssafy/memberservice/domain/chatting/service/PloggingChatService.java
@@ -8,6 +8,7 @@ import com.ssafy.memberservice.domain.chatting.domain.PloggingChatRoom;
 import com.ssafy.memberservice.domain.chatting.domain.enums.Color;
 import com.ssafy.memberservice.domain.chatting.domain.enums.MessageType;
 import com.ssafy.memberservice.domain.chatting.domain.enums.PingType;
+import com.ssafy.memberservice.domain.chatting.dto.ActivateCrewPloggingResponse;
 import com.ssafy.memberservice.domain.chatting.dto.PloggingChatMessage;
 import com.ssafy.memberservice.domain.chatting.dto.PloggingChatRoomResponse;
 import com.ssafy.memberservice.domain.member.dao.MemberRepository;
@@ -22,6 +23,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.*;
 
 import static com.ssafy.memberservice.global.common.error.exception.NotFoundException.JOINWAITING_NOT_FOUND;
@@ -30,6 +32,7 @@ import static com.ssafy.memberservice.global.common.error.exception.NotFoundExce
 @Slf4j
 @RequiredArgsConstructor
 @Service
+@Transactional
 public class PloggingChatService {
     private final RedisTemplate redisTemplate;
 
@@ -61,7 +64,7 @@ public class PloggingChatService {
         return PloggingChatRoomResponse.of(ploggingChatRoom);
     }
 
-    public void joinRoom(Member member, String roomId) {
+    public PloggingChatRoom joinRoom(Member member, String roomId) {
         PloggingChatRoom ploggingChatRoom = ploggingChatRepository.findById(roomId).orElseThrow(() -> new NotFoundException("해당 방이 존재하지 않습니다."));
 
 
@@ -72,12 +75,12 @@ public class PloggingChatService {
             ploggingChatRoom.setPlayerMap(new LinkedHashMap<>());
         }
         ploggingChatRoom.getPlayerMap().put(member.getId().toString(), participant);
-        ploggingChatRepository.save(ploggingChatRoom);
+        return ploggingChatRoom;
     }
 
     private Color getNewColor(PloggingChatRoom ploggingChatRoom) {
         Set<Color> usedColors = new HashSet<>();
-        if (ploggingChatRoom.getPlayerMap() == null ||ploggingChatRoom.getPlayerMap().size() == 0) return Color.RED;
+        if (ploggingChatRoom.getPlayerMap() == null || ploggingChatRoom.getPlayerMap().size() == 0) return Color.RED;
         for (Participant participant : ploggingChatRoom.getPlayerMap().values()) {
             usedColors.add(Color.valueOf(participant.getColor()));
         }
@@ -113,17 +116,19 @@ public class PloggingChatService {
             } else {
 
             }
+        } else {
+
         }
 
-
-            redisTemplate.convertAndSend(ploggingTopic.getTopic(), chatMessage);
-
+        redisTemplate.convertAndSend(ploggingTopic.getTopic(), chatMessage);
     }
 
     public void sendChatMessage(PloggingChatMessage chatMessage, String memberId) {
 //        chatMessage.setUserCount(chatRoomRepository.getUserCount(chatMessage.getRoomId()));
-        Member member = memberRepository.findById(UUID.fromString(memberId)).orElseThrow(() -> new NotFoundException(USER_NOT_FOUND));
-        chatMessage.setSender(Participant.from(member));
+//        Member member = memberRepository.findById(UUID.fromString(memberId)).orElseThrow(() -> new NotFoundException(USER_NOT_FOUND));
+        PloggingChatRoom ploggingChatRoom = ploggingChatRepository.findById(chatMessage.getRoomId()).orElseThrow(() -> new NotFoundException("해당하는 방을 찾을 수 없습니다."));
+        chatMessage.setSender(ploggingChatRoom.getPlayerMap().get(memberId));
+
         sendChatMessage(chatMessage);
     }
 
@@ -141,5 +146,19 @@ public class PloggingChatService {
         PloggingChatRoom ploggingChatRoom = ploggingChatRepository.findByCrewId(crewId).orElseThrow(() -> new NotFoundException("크루 플로깅 방이 없습니다."));
 
         return PloggingChatRoomResponse.of(ploggingChatRoom);
+    }
+
+    public List<ActivateCrewPloggingResponse> getCrewPloggingListFromMemberId(String memberId) {
+        List<MemberCrew> memberCrewListByMemberId = memberCrewRepository.findMemberCrewListByMemberId(UUID.fromString(memberId));
+        List<ActivateCrewPloggingResponse> activateCrewPloggingResponses = new ArrayList<>();
+        memberCrewListByMemberId.forEach(memberCrew -> {
+            Optional<PloggingChatRoom> byCrewId = ploggingChatRepository.findByCrewId(memberCrew.getId());
+            if (byCrewId.isPresent()) {
+                PloggingChatRoom ploggingChatRoom = byCrewId.get();
+                activateCrewPloggingResponses.add(ActivateCrewPloggingResponse.of(ploggingChatRoom.getRoomId(), memberCrew.getCrew().getName(), memberCrew.getId()));
+            }
+        });
+
+        return activateCrewPloggingResponses;
     }
 }
