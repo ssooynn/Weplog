1dbc0afaeb603b27d42f21553f84c0f86113e89e
rjeowornjs2 <rjeowornjs@naver.com>
2022-11-10 17:00:04 +0900

    test : 크루 찹가 동시성 테스트

---
+++ backend/member-service/src/test/java/com/ssafy/memberservice/domain/crew/TestInfo.java
@@ -0,0 +1,9 @@
+package com.ssafy.memberservice.domain.crew;
+
+import com.ssafy.memberservice.domain.crew.domain.Crew;
+import com.ssafy.memberservice.domain.joinwaiting.domain.JoinWaiting;
+import com.ssafy.memberservice.domain.membercrew.domain.MemberCrew;
+
+public class TestInfo {
+
+}
