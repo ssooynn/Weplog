16d7df8a55f0163263ccd9eb6ec7815cf0e0e64b
문석희 <zazachucky@naver.com>
2022-11-10 15:52:16 +0900

    fix: 자바 Reflection 적용되게 기본 생성자 추가

--- backend/member-service/src/main/java/com/ssafy/memberservice/domain/calendar/dto/CalendarReq.java
+++ backend/member-service/src/main/java/com/ssafy/memberservice/domain/calendar/dto/CalendarReq.java
@@ -6,6 +6,7 @@ import com.ssafy.memberservice.domain.member.domain.Member;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -13,6 +14,7 @@ import java.util.UUID;
 
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class CalendarReq {
     Long crewId;
