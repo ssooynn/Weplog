83f6810c5d5a9de101551a64a13f9de64dc0b103
rjeowornjs2 <rjeowornjs@naver.com>
2022-11-11 14:19:30 +0900

    test : 크루 참가 동시성 문제 해결

--- backend/member-service/src/test/java/com/ssafy/memberservice/domain/crew/TestInfo.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.memberservice.domain.crew;
-
-import com.ssafy.memberservice.domain.crew.domain.Crew;
-import com.ssafy.memberservice.domain.joinwaiting.domain.JoinWaiting;
-import com.ssafy.memberservice.domain.membercrew.domain.MemberCrew;
-
-public class TestInfo {
-
-}
