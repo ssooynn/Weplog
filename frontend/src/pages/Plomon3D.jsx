125cba26aa4fca41552fb626c98068203c4775ca
문석희 <zazachucky@naver.com>
2022-11-16 16:59:21 +0900

    fix : 플로몬 애니메이션 작동

--- frontend/src/pages/Plomon3D.jsx
+++ frontend/src/pages/Plomon3D.jsx
@@ -1,6 +1,6 @@
 import React, { Suspense, useRef, useEffect, useState } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls } from "@react-three/drei";
 import { Plomon3DDetail } from "../components/main/Plomon3DDetail";
 import { useNavigate, useLocation } from "react-router-dom";
 import styled from "styled-components";
@@ -8,8 +8,7 @@ import BackArrowIcon from "../assets/icons/backArrowIcon.svg";
 import CameraIcon from "../assets/icons/cameraIcon.svg";
 import HeartIcon from "../assets/icons/heartIcon.svg";
 import { Box } from "grommet";
-import { getMyPetDetail } from '../apis/memberPetApi'
-
+import { getMyPetDetail } from "../apis/memberPetApi";
 
 const PlomonTableTitle = styled.div`
   padding-top: 8%;
@@ -20,7 +19,7 @@ const PlomonTableTitle = styled.div`
   color: #232323;
   display: flex;
   align-items: center;
-`
+`;
 
 const PlomonContentsArea = styled.div`
   margin-top: 58vh;
@@ -30,21 +29,21 @@ const PlomonContentsArea = styled.div`
   flex-direction: column;
   justify-content: center;
   z-index: 1;
-`
+`;
 
 const PlomonContentsName = styled.div`
   font-size: 18px;
   font-weight: bold;
   color: #232323;
   margin-bottom: 2vh;
-`
+`;
 
 const PlomonContentsDescription = styled.div`
   font-size: 16px;
   font-weight: 400;
   color: #232323;
   line-height: 150%;
-`
+`;
 
 const PlomonContentsButtonsArea = styled.div`
   margin-top: 5vh;
@@ -52,26 +51,37 @@ const PlomonContentsButtonsArea = styled.div`
   width: 100vw;
   flex-direction: row;
   justify-content: center;
-`
+`;
 
 const PlomonContentsButton = styled.div`
   height: 4vh;
   margin: 0 5vw;
-  
-`
+`;
 
 function Plomon(props) {
   const mesh = useRef(null);
+  const ref = useRef(null);
   const [animationIndex, setAnimationIndex] = useState([2, 9, 0, 3, 17, 13]);
   const [index, setIndex] = useState(0);
+
+  const handlePlomonClick = () => {
+    setIndex((index + 1) % 6);
+    ref.current.handleAnimation(index);
+  };
+
   useFrame(() => (mesh.current.rotation.y = mesh.current.rotation.y += 0.0005));
-  useEffect(() => {
-    console.log("test");
-  }, [index])
+  // useEffect(() => {
+  //   console.log("test");
+  // }, [index]);
   return (
     <mesh ref={mesh} scale={0.5} position={[0, 1, 0]}>
-      <Plomon3DDetail name={props.name} scale={0.4} position={[0, 0, 0]} animationIndex={animationIndex[index]}
-      // onClick={() => (setIndex((index+1)%6))} //클릭하면 애니매이션 변경
+      <Plomon3DDetail
+        ref={ref}
+        name={props.name}
+        scale={0.4}
+        position={[0, 0, 0]}
+        // animationIndex={animationIndex[index]}
+        onClick={handlePlomonClick} //클릭하면 애니매이션 변경
       />
       <meshLambertMaterial attach="material" />
     </mesh>
@@ -84,7 +94,6 @@ export const Plomon3D = () => {
   const gottenPetId = location.state.gottenPetId;
   const [myPetDetail, setMyPetDetail] = useState([]);
 
-
   useEffect(() => {
     getMyPetDetail(
       gottenPetId,
@@ -95,14 +104,9 @@ export const Plomon3D = () => {
         console.log(err);
       }
     );
+  }, []);
 
-  }, [])
-
-  useEffect(() => {
-
-  }, [])
-
-
+  useEffect(() => {}, []);
 
   // const plomonRef = useRef();
 
@@ -136,20 +140,21 @@ export const Plomon3D = () => {
         position: "relative",
         direction: "column",
       }}
-
     >
-      <Box
-        width="100%"
-        style={{ position: "absolute", zIndex: 3 }}
-      >
+      <Box width="100%" style={{ position: "absolute", zIndex: 3 }}>
         <PlomonTableTitle onClick={() => navigate("/main")}>
-          <img style={{ width: "30px", height: "30px", paddingRight: "20px" }} src={BackArrowIcon} />
+          <img
+            style={{ width: "30px", height: "30px", paddingRight: "20px" }}
+            src={BackArrowIcon}
+          />
           {myPetDetail.name}
         </PlomonTableTitle>
       </Box>
       <PlomonContentsArea>
         <PlomonContentsName>{myPetDetail.name}</PlomonContentsName>
-        <PlomonContentsDescription>{myPetDetail.description}</PlomonContentsDescription>
+        <PlomonContentsDescription>
+          {myPetDetail.description}
+        </PlomonContentsDescription>
         <PlomonContentsButtonsArea>
           <PlomonContentsButton>
             <img src={CameraIcon} />
@@ -158,14 +163,15 @@ export const Plomon3D = () => {
             <img src={HeartIcon} />
           </PlomonContentsButton>
         </PlomonContentsButtonsArea>
-
       </PlomonContentsArea>
-      <Canvas
-        camera={{ position: [0, 10, 40] }}
-      >
+      <Canvas camera={{ position: [0, 10, 40] }}>
         <Suspense fallback={null}>
           <Plomon name={myPetDetail.name} />
-          <OrbitControls enableZoom={false} maxPolarAngle={1.5} minPolarAngle={1.5} />
+          <OrbitControls
+            enableZoom={false}
+            maxPolarAngle={1.5}
+            minPolarAngle={1.5}
+          />
           <directionalLight color={"white"} position={[0, 10, 10]} />
           <directionalLight color={"white"} position={[-8.6, 10, -5]} />
           <directionalLight color={"white"} position={[8.6, 10, -5]} />
@@ -174,4 +180,4 @@ export const Plomon3D = () => {
       </Canvas>
     </div>
   );
-}
+};
