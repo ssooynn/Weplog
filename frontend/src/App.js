8d2184e0f09a2ae52a5008ddedd287b72bec2a88
qospwmf <qospwmf@gmail.com>
2022-11-16 13:30:37 +0900

    feat: SSE 적용 초기

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,48 @@
 import "./App.css";
-import { Router } from "./Router";
+import React, {useEffect, useState} from "react";
+import {Router} from "./Router";
+import {Grommet, Notification} from "grommet";
+import {GrommetTheme} from "./utils/util";
+import {API_SERVER} from "./apis/api";
+import {useSelector} from "react-redux";
+import {EventSourcePolyfill} from 'event-source-polyfill';
 
 function App() {
+  const [visible, setVisible] = useState(false);
+  const User = useSelector((state) => state.user.user);
+  let eventSource = null;
+  useEffect(() => {
+    if (User !== undefined) {
+      eventSource = new EventSourcePolyfill(`${API_SERVER}member-service/notification/subscribe`, {
+        headers: {
+          'memberId': localStorage.getItem("memberId"),
+          'Authorization': `Bearer ${localStorage.getItem("accessToken")}`,
+
+        }
+      });
+      eventSource.addEventListener("sse", function (event) {
+        console.log(event.data);
+
+        const data = JSON.parse(event.data);
+
+      });
+    }
+  }, [User])
+
+
   return (
     <div className="App">
-      <Router />
+      {visible && (
+        <Grommet theme={GrommetTheme}>
+          <Notification
+            toast={{position: "center"}}
+            title={"새 마커가 등록되었습니다."}
+            status={"normal"}
+            onClose={() => setVisible(false)}
+          />
+        </Grommet>
+      )}
+      <Router/>
     </div>
   );
 }
