4927f797678ca94aa2924186c9c40febfc4f2775
ahyun98 <dldkgus98@naver.com>
2022-11-16 17:24:01 +0900

    feat: main switch 추가

--- frontend/src/components/main/MainMYContents.jsx
+++ frontend/src/components/main/MainMYContents.jsx
@@ -1,22 +1,24 @@
 import React, { Suspense, useRef, useState, useEffect } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import {OrbitControls} from '@react-three/drei';
+import { OrbitControls } from '@react-three/drei';
 import { SnowIsland } from "./Snow_island";
 import { Plomon } from "./Plomon";
 import { getAllMyPet } from '../../apis/memberPetApi'
 
 function getRandomIndexList(num) {
   if (num) {
-      let indexs = []
-      for (let i=0; i<num; i++) {
-        indexs.push(i);};
-      const randomIndexList = [];
-      while(indexs.length > 0){
-        let movenum = indexs.splice(Math.floor(Math.random() * indexs.length),1)[0];
-        randomIndexList.push(movenum);};
-        return randomIndexList;
-    } else { return ''}
-  };
+    let indexs = []
+    for (let i = 0; i < num; i++) {
+      indexs.push(i);
+    };
+    const randomIndexList = [];
+    while (indexs.length > 0) {
+      let movenum = indexs.splice(Math.floor(Math.random() * indexs.length), 1)[0];
+      randomIndexList.push(movenum);
+    };
+    return randomIndexList;
+  } else { return '' }
+};
 
 function Island(props) {
   const mesh = useRef(null);
@@ -24,9 +26,10 @@ function Island(props) {
   const [plomonStates, setPlomonStates] = useState([[[0, 23, 11], [0, -90, 0], 0, 0.02, 9], [[-8, 15.2, 26], [0, -0.4, 0], 0, 0, 0], [[-20, 14, 13], [0, -1, 0], 0, 0, 3], [[-21, 15.2, -7], [0, -2, 0], 0, 0, 2], [[-7, 15, -21], [0, -2.6, 0], 0, 0, 17], [[22, 38, 0], [0, 0, 0], 0.5, 0, 13]]); //[position, rotation, speed, rSpeed, animationIndex]
   const [randomIndexList, setRandomIndexList] = useState([]);
   useFrame(() => (mesh.current.rotation.y = mesh.current.rotation.y += 0.0005));
-  useEffect(()=>{
+  useEffect(() => {
     getAllMyPet(
       (res) => {
+        console.log(res);
         setAllMyPet(res.data);
         setRandomIndexList(getRandomIndexList(res.data.length));
       },
@@ -34,16 +37,16 @@ function Island(props) {
         console.log(err);
       }
     );
-  },[])
+  }, [])
   return (
     <mesh ref={mesh} scale={0.5} position={[0, -5, 0]}>
       <SnowIsland />
-        {allMyPet!==undefined && allMyPet.length>0 && allMyPet.map((pet, idx)=>(
-          randomIndexList[idx] < 6 ?
-          <Plomon key={idx} name={pet.name} position={plomonStates[randomIndexList[idx]][0]} rotation={plomonStates[randomIndexList[idx]][1]} speed={plomonStates[randomIndexList[idx]][2]} rSpeed={plomonStates[randomIndexList[idx]][3]} animationIndex={plomonStates[randomIndexList[idx]][4]} scale={0.08} onClick={() => (props.setPlomonOpen(true), props.setIsPlomonClicked(true), props.setTargetPlomon(pet))}/>
+      {allMyPet !== undefined && allMyPet.length > 0 && allMyPet.map((pet, idx) => (
+        randomIndexList[idx] < 6 ?
+          <Plomon key={idx} name={pet.name} position={plomonStates[randomIndexList[idx]][0]} rotation={plomonStates[randomIndexList[idx]][1]} speed={plomonStates[randomIndexList[idx]][2]} rSpeed={plomonStates[randomIndexList[idx]][3]} animationIndex={plomonStates[randomIndexList[idx]][4]} scale={0.08} onClick={() => (props.setPlomonOpen(true), props.setIsPlomonClicked(true), props.setTargetPlomon(pet))} />
           : console.log('no')
-        )
-        )}
+      )
+      )}
       <meshLambertMaterial attach="material" />
     </mesh>
   );
@@ -52,11 +55,11 @@ function Island(props) {
 export function MainMYContents(props) {
   return (
     <Canvas
-    camera={{position: [-40, 15, 0]}}
+      camera={{ position: [-40, 15, 0] }}
     >
       <Suspense fallback={null}>
-        <Island setPlomonOpen={props.setPlomonOpen} setIsPlomonClicked={props.setIsPlomonClicked} setTargetPlomon={props.setTargetPlomon}/>
-        <OrbitControls minPolarAngle={0.5} maxPolarAngle={1.5} minDistance={20} maxDistance={50}/>
+        <Island setPlomonOpen={props.setPlomonOpen} setIsPlomonClicked={props.setIsPlomonClicked} setTargetPlomon={props.setTargetPlomon} />
+        <OrbitControls minPolarAngle={0.5} maxPolarAngle={1.5} minDistance={20} maxDistance={50} />
         <directionalLight color={"white"} position={[0, 10, 10]} />
         <directionalLight color={"white"} position={[-8.6, 10, -5]} />
         <directionalLight color={"white"} position={[8.6, 10, -5]} />
