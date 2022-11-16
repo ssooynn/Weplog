926007061902b02e03550f2631970634f8e554f9
rjeowornjs2 <rjeowornjs@naver.com>
2022-11-16 14:54:56 +0900

    fix : 크루 플로깅 기록 조회 및 크루 중복 신청 수정

--- backend/member-service/src/main/java/com/ssafy/memberservice/domain/calendar/repository/CalendarRepository.java
+++ backend/member-service/src/main/java/com/ssafy/memberservice/domain/calendar/repository/CalendarRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 
 public interface CalendarRepository extends JpaRepository<Calendar, Long> {
 
-    @Query(value= "select c from Calendar c where c.crew.id = :crewId and c.scheduleDate between :start and :end")
+    @Query(value= "select c from Calendar c where c.crew.id = :crewId and c.scheduleDate between :start and :end order by c.scheduleDate desc")
     List<Calendar> findAllByDate(LocalDateTime start, LocalDateTime end, Long crewId);
 }
