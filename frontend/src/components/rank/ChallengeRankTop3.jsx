0dca3f3bffec382a06fcb34c51669b8e84412c0c
ahyun98 <dldkgus98@naver.com>
2022-11-17 13:32:50 +0900

    feat: 랭킹킹 수정

--- frontend/src/components/rank/ChallengeRankTop3.jsx
+++ frontend/src/components/rank/ChallengeRankTop3.jsx
@@ -79,7 +79,19 @@ export const ChallengeRankTop3 = ({ type, top3, rankType }) => {
           return undefined;
       }
     } else {
-      return top3[id].totalAmount;
+      switch (rankType) {
+        case "DISTANCE":
+          return (Number(top3[id].totalAmount) * 0.001).toFixed(2) + " Km";
+
+        case "PLOGGING_CNT":
+          return top3[id].totalAmount + " 회";
+
+        case "TIME":
+          return secChangeTime(top3[id].totalAmount);
+
+        default:
+          return undefined;
+      }
     }
   }
   if (top3 && top3.length > 0)
@@ -112,12 +124,14 @@ export const ChallengeRankTop3 = ({ type, top3, rankType }) => {
           align="center"
           style={{ position: "relative" }}
         >
-          <img
-            src={crown}
-            alt="왕관 사진"
-            style={{ position: "absolute", top: "-20px", left: "30px" }}
-          />
-          <Profile1st src={changeImage(0)} alt="프로필 사진" />
+          <Box>
+            <Profile1st src={changeImage(0)} alt="프로필 사진" />
+            <img
+              src={crown}
+              alt="왕관 사진"
+              style={{ position: "absolute", top: "-10%", left: "39%" }}
+            />
+          </Box>
           <Box direction="column" align="center">
             <Text size="10px" weight={500}>
               1등
