d482937b24cb32c7acb1ca2d1ab71e64bb4af1b3
ahyun98 <dldkgus98@naver.com>
2022-11-17 17:40:00 +0900

    fix : 시간 포맷 수정

--- frontend/src/components/rank/ChallengeRankCardList.jsx
+++ frontend/src/components/rank/ChallengeRankCardList.jsx
@@ -39,13 +39,13 @@ export const ChallengeRankCardList = (data) => {
         var hour = parseInt(seconds / 3600);
         var min = parseInt((seconds % 3600) / 60);
         var sec = seconds % 60;
-        if (hour <= 10) {
+        if (hour < 10) {
             hour = `0${hour}`;
         }
-        if (min <= 10) {
+        if (min < 10) {
             min = `0${min}`;
         }
-        if (sec <= 10) {
+        if (sec < 10) {
             sec = `0${sec}`;
         }
         return `${hour} : ${min} : ${sec}`;
